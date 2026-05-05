791966df1f65644f80d96f543dd915da1c13d667
Paul Péret <thehivemc1234@gmail.com>
2026-05-05 01:52:56 +0100

    feat: annealing debug

--- train_vae.py
+++ train_vae.py
@@ -91,6 +91,7 @@ def train_vae(epochs, batch_size, bottleneck_size, output_file, dataset, learnin
                 # Variational encoders add a regularization term that computes the KL divergence between the encoder
                 # distribution and the normal distribution
                 loss += kl_annealing(epoch, epochs, 1) * regularization_loss(mean, std)
+                print(kl_annealing(epoch, epochs, 1))
                 running_regularization_loss += loss.item() - current_reconstruction_loss
 
             scaler.scale(loss).backward()
