fix: potential undesirable computation of gradient in the sampling operation

models/variational_autoencoder.py
@@ -110,7 +110,7 @@ class Encoder(nn.Module):
         std = self.std(block_5.squeeze()).exp().sqrt()
 
         # Reparametrization trick:
-        z = mean + std * torch.randn_like(std)
+        z = mean + std * torch.randn_like(std.detach())
 
         return z, mean, std
     

train_vae.py
@@ -49,7 +49,7 @@ def train_vae(epochs, batch_size, bottleneck_size, output_file, dataset, learnin
         return torch.mean(-0.5*torch.sum((1 + torch.log(std.square()) - mean.square() - std.square()), dim=-1))
 
     def kl_annealing(epoch, epochs, beta):
-        offset = int(epochs*10/100)
+        offset = int(epochs*20/100)
         return beta/(1+math.exp(-(epoch-offset)))
     
     min_val_loss = math.inf