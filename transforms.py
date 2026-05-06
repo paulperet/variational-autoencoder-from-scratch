78b583eacf4697e218299ebf00df4493dbdff33a
Paul Péret <thehivemc1234@gmail.com>
2026-05-06 20:00:08 +0100

    fix: resize after randomcrop

--- transforms.py
+++ transforms.py
@@ -2,8 +2,8 @@ from torchvision.transforms import transforms
 
 # Augment train data
 train_transforms = transforms.Compose([
-    transforms.RandomCrop((224,224)),
     transforms.Resize((224, 224)),
+    transforms.RandomCrop((224,224)),
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
