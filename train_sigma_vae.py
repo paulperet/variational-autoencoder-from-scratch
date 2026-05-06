fix: no gradient from log_sigma and added softclip following paper implementation

train_sigma_vae.py
@@ -47,13 +47,17 @@ def train_sigma_vae(epochs, batch_size, bottleneck_size, output_file, dataset, l
 
     def regularization_loss(mean, std):
         return -0.5*torch.sum((1 + torch.log(std.square()) - mean.square() - std.square()))
+    
+    def softclip(tensor, min):
+        result_tensor = min + torch.nn.F.softplus(tensor - min)
+        return result_tensor
 
     def gaussian_nll(mu, log_sigma, x):
         return 0.5 * torch.pow((x - mu) / log_sigma.exp(), 2) + log_sigma + 0.5 * np.log(2 * np.pi)
     
     def reconstruction_loss(y, x):
-        log_sigma = ((x - y) ** 2).mean([0,1,2,3], keepdim=True).sqrt().log().item() # No backpropagation
-        log_sigma = torch.clip(torch.tensor(log_sigma), min=-6)
+        log_sigma = ((x - y) ** 2).mean([0,1,2,3], keepdim=True).sqrt().log()
+        log_sigma = softclip(torch.tensor(log_sigma), -6)
         loss = gaussian_nll(y, log_sigma, x).sum()
 
         return loss