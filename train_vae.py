28e6b85c1e5b7be8d98fbbf7b78bde9c62e26b2b
Paul Péret <thehivemc1234@gmail.com>
2026-05-08 15:17:56 +0100

    feat: mean per batch for rec and kl loss to avoid too high gradient/nan values

--- train_vae.py
+++ train_vae.py
@@ -44,10 +44,10 @@ def train_vae(epochs, batch_size, bottleneck_size, output_file, dataset, learnin
     scaler = torch.amp.GradScaler("cuda" ,enabled=use_amp)
     scheduler = ReduceLROnPlateau(optimizer, factor=1e-1, patience=5)
 
-    reconstruction_loss = nn.MSELoss(reduction='sum')
+    reconstruction_loss = nn.MSELoss(reduction='none')
 
     def regularization_loss(mean, std):
-        return -0.5*torch.sum(1 + torch.log(std.square()) - mean.square() - std.square())
+        return -0.5*torch.sum(1 + torch.log(std.square()) - mean.square() - std.square(), dim=1)
     
     min_val_loss = math.inf
 
@@ -79,14 +79,14 @@ def train_vae(epochs, batch_size, bottleneck_size, output_file, dataset, learnin
                 # Pass our input through the model to get our output
                 outputs, mean, std = model(inputs)
 
-                loss = reconstruction_loss(outputs, inputs)
+                loss = reconstruction_loss(outputs, inputs).sum([1,2,3]).mean()
 
                 current_reconstruction_loss = loss.item()
                 running_reconstruction_loss += loss.item()
 
                 # Variational encoders add a regularization term that computes the KL divergence between the encoder
                 # distribution and the normal distribution
-                loss += regularization_loss(mean, std)
+                loss += regularization_loss(mean, std).mean()
                 running_regularization_loss += loss.item() - current_reconstruction_loss
 
             scaler.scale(loss).backward()
